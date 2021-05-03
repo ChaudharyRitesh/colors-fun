f643bdcf9c4881002b7c74b0c605c552207deb36
Ritesh Chaudhary <ritesh.chaudhari.t5a17@icp.edu.np>
2021-05-03 10:59:42 +0545

    fixed css margin issuse

--- src/Navbar.js
+++ src/Navbar.js
@@ -30,7 +30,7 @@ class Navbar extends Component {
     return (
       <header className={classes.Navbar}>
         <div className={classes.logo}>
-          <Link to="/">colors-fun</Link>
+          <Link to="/">colorsfun</Link>
         </div>
         {showingAllColors && (
           <div>
